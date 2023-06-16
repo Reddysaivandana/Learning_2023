6a341a9081ea328b3e812079913658198fcda3b6
Reddysaivandana <136626435+Reddysaivandana@users.noreply.github.com>
2023-06-16 12:04:15 +0000

    add day three solution of L1 activity

--- module1/day3/Exp1.c
+++ module1/day3/Exp1.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 
-void print_numbers(int n) {
-    int i;
-    for (i = 1; i <= n; i++) {
-        printf("%d", i);
-    }
-    for (i = n - 1; i >= 1; i--) {
+void printNumbersAscending(int n) {
+    for (int i = 1; i <= n; i++) {
         printf("%d", i);
     }
 }
 
-void print_spaces(int n) {
-    int i;
-    for (i = 1; i <= n - 2; i++) {
-        printf(" ");
+void printNumbersDescending(int n) {
+    for (int i = n; i >= 1; i--) {
+        printf("%d", i);
     }
 }
 
-void print_pattern(int n) {
-    int i;
-    for (i = n; i >= 1; i--) {
-        print_numbers(i);
-        if (i > 1) {
-            print_spaces(i);
+void printPattern(int n) {
+    for (int i = n; i >= 1; i--) {
+        printNumbersAscending(i);
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
         }
-        print_numbers(i);
+        printNumbersDescending(i);
         printf("\n");
     }
 }
 
 int main() {
-    int n;
-    printf("Enter the value of n: ");
-    scanf("%d", &n);
-    print_pattern(n);
+    
+    int a;
+    printf("\n Enter a number:");
+    scanf("%d",&a);
+    printPattern(a);
     return 0;
 }
